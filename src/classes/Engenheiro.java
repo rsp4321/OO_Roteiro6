47321efa54172c588981b65bfdadea89432f217d
Rodrigo Silva Pinto <rsp.4321@gmail.com>
2019-05-13 21:44:00 -0300

    Ajustado o método "Engenheiro.getBonificacao()".

--- src/classes/Engenheiro.java
+++ src/classes/Engenheiro.java
@@ -1,23 +1,48 @@
 package classes;
 
-public class Engenheiro extends Funcionario{
-    
-    public Engenheiro(){
-        super();       
+public class Engenheiro extends Funcionario {
+
+    private final static float ALIQUOTA_BONIFICACAO = 8;  // Fixando a alíquota de bonificação para clarear
+
+    public Engenheiro() {
+        super();
     }
-    
-    public void preencher(String nome, char sexo, int idade, float horasTrabalho, float salario){
+
+    public void preencher(String nome, char sexo, int idade, float horasTrabalho, float salario) {
         super.preencher(nome, sexo, idade, horasTrabalho, salario);
     }
-    
-    public String imprime(){
-        String str = super.imprime() + "\nBonificação: " 
-                    + getBonificacao((int) this.horasTrabalho, this.salario);
+
+    public String imprime() {
+        
+        String str = super.imprime() + "\nBonificação: "
+//                + getBonificacao((int) this.horasTrabalho, this.salario);
+                + this.getBonificacao();
+        
         return str;
     }
-    
-    public double getBonificacao(int horasTrabalho, float salario){
+
+    public double getBonificacao(int horasTrabalho, float salario) {
         double bonificacao = super.getBonificacao(horasTrabalho, salario, 8);
+        return bonificacao;
+    }
+
+    public double getBonificacao() {
+
+        double bonificacao = 0;
+
+//        int horasExtras = horasTrabalho - 44;
+        float horasExtras = this.getHorasTrabalho() - Funcionario.CH_MAXIMA_SEMANAL_CLT; // Tive que colocar como float pois o código está todo implementado em float
+
+//        taxa = taxa / 100;
+//        float taxa = Funcionario.ALIQUOTA_BONIFICACAO / 100;
+        float taxa = Engenheiro.ALIQUOTA_BONIFICACAO / 100;
+
+        if (horasExtras > 0) {
+            bonificacao = salario * taxa * horasExtras;
+        } else {
+            bonificacao = 0;
+        }
+
         return bonificacao;
     }
 }
